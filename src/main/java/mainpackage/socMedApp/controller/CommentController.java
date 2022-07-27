9cdc29f598962f6be359ef61d8a83eeaeb6fbcb3
tj-dsk-tpp <tushar01.tjdsk@gmail.com>
2022-07-28 00:26:50 +0530

    comment controller revision 1

--- src/main/java/mainpackage/socMedApp/controller/CommentController.java
+++ src/main/java/mainpackage/socMedApp/controller/CommentController.java
@@ -1,36 +1,42 @@
 package mainpackage.socMedApp.controller;
 
-import mainpackage.socMedApp.model.comment.Comment;
-import mainpackage.socMedApp.model.comment.CommentResponse;
-import mainpackage.socMedApp.model.comment.DeleteCommentRequest;
-import mainpackage.socMedApp.model.comment.EditCommentRequest;
+import mainpackage.socMedApp.model.comment.*;
+import mainpackage.socMedApp.model.post.EditPostResponse;
 import mainpackage.socMedApp.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
-    @Autowired
-    CommentService commentService;
+	@Autowired
+	CommentService commentService;
 
-    @PostMapping(value = "/post_comment", consumes = "application/json", produces = "application/json")
-    public CommentResponse postComment(@RequestBody Comment comment) {
-
-        CommentResponse commentResponse = commentService.register(comment);
-        return commentResponse;
-
-    }
+	@PostMapping(value = "/comment", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<CommentResponse> postComment(@RequestBody Comment comment) {
+		CommentResponse commentResponse = commentService.postComment(comment);
+		return new ResponseEntity<>(commentResponse, commentResponse.isStatus() ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST);
+	}
 
 
-    @DeleteMapping(value = "/delete_comment", consumes = "application/json", produces = "application/json")
-    public void deletecomment(@RequestBody DeleteCommentRequest deleteCommentRequest) {
-        commentService.delete(deleteCommentRequest);
+	@DeleteMapping(value = "/comment/{commentId}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<DeleteCommentResponse> deleteComment(@PathVariable("commentId") String commentId, @RequestBody DeleteCommentRequest deleteCommentRequest) {
+		DeleteCommentResponse deleteCommentResponse = commentService.deleteComment(commentId, deleteCommentRequest);
+		return new ResponseEntity<>(deleteCommentResponse, deleteCommentResponse.isStatus() ? HttpStatus.OK : HttpStatus.UNAUTHORIZED);
+	}
 
-    }
+	@PutMapping(value = "/comment/{commentId}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<EditCommentResponse> editComment(@PathVariable("commentId") String commentId, @RequestBody EditCommentRequest editCommentRequest) {
+		EditCommentResponse editCommentResponse = commentService.editComment(commentId, editCommentRequest);
+		return new ResponseEntity<>(editCommentResponse, editCommentResponse.isStatus() ? HttpStatus.OK : HttpStatus.UNAUTHORIZED);
+	}
 
-    @PutMapping(value = "/edit_comment", consumes = "application/json", produces = "application/json")
-    public void editcomment(@RequestBody EditCommentRequest editCommentRequest) {
-        commentService.edit_comment(editCommentRequest);
-
-    }
+	@GetMapping(value = "/post/{postId}/comments")
+	public ResponseEntity<List<CommentBanners>> getCommentsByPostId(@PathVariable("postId") String postId) {
+		List<CommentBanners> commentBannersList = commentService.getCommentsByPostId(postId);
+		return new ResponseEntity<>(commentBannersList, commentBannersList == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+	}
 }
