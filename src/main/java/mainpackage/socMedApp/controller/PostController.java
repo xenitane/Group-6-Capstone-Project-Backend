d032acc56c1df0e8652481bf04cf76108e7803ac
tj-dsk-tpp <tushar01.tjdsk@gmail.com>
2022-07-29 12:26:18 +0530

    making some headers nullable

--- src/main/java/mainpackage/socMedApp/controller/PostController.java
+++ src/main/java/mainpackage/socMedApp/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,43 +18,43 @@ import java.util.List;
 public class PostController {
 	@Autowired
 	PostService postService;
-
+	
 	@PostMapping(value = "/post", consumes = "application/json")
 	public ResponseEntity<PostResponse> makePost(@RequestBody Post post) {
 		Pair<PostResponse, HttpStatus> postResponse = postService.savePost(post);
 		return new ResponseEntity<>(postResponse.getFirst(), postResponse.getSecond());
 	}
-
+	
 	@GetMapping(value = "/post/{postId}")
-	public ResponseEntity<GetPostByIdResponse> getPost(@PathVariable String postId, @RequestHeader("currentUserId") String currentUserId) {
+	public ResponseEntity<GetPostByIdResponse> getPost(@PathVariable String postId, @Nullable @RequestHeader("currentUserId") String currentUserId) {
 		Pair<GetPostByIdResponse, HttpStatus> getPostByIdResponse = postService.getPostById(postId, currentUserId);
 		return new ResponseEntity<>(getPostByIdResponse.getFirst(), getPostByIdResponse.getSecond());
 	}
-
+	
 	@DeleteMapping(value = "/post/{postId}")
 	public ResponseEntity<DeletePostResponse> deletePost(@PathVariable String postId, @RequestBody DeletePostRequest deletePostRequest) {
 		Pair<DeletePostResponse, HttpStatus> deletePostResponse = postService.deletePost(postId, deletePostRequest);
 		return new ResponseEntity<>(deletePostResponse.getFirst(), deletePostResponse.getSecond());
 	}
-
+	
 	@PutMapping(value = "/post/{postId}")
 	public ResponseEntity<EditPostResponse> editPost(@PathVariable("postId") String postId, @RequestBody EditPostRequest editPostRequest) {
 		Pair<EditPostResponse, HttpStatus> editPostResponse = postService.editPost(postId, editPostRequest);
 		return new ResponseEntity<>(editPostResponse.getFirst(), editPostResponse.getSecond());
 	}
-
-	@GetMapping(value = "/{username}/posts")
-	public ResponseEntity<List<PostBody>> getPostsByUsername(@PathVariable("username") String username, @RequestHeader("currentUserId") String currentUserId) {
+	
+	@GetMapping(value = "/user/{username}/posts")
+	public ResponseEntity<List<PostBody>> getPostsByUsername(@PathVariable("username") String username,@Nullable @RequestHeader("currentUserId") String currentUserId) {
 		Pair<List<PostBody>, HttpStatus> postBodyList = postService.getPostsByUsername(username, currentUserId);
 		return new ResponseEntity<>(postBodyList.getFirst(), postBodyList.getSecond());
 	}
-
+	
 	@PatchMapping(value = "/post/{postId}/react")
 	public ResponseEntity<ReactResponse> reactOnPost(@PathVariable("postId") String postId, @RequestBody ReactRequest reactRequest) {
 		Pair<ReactResponse, HttpStatus> reactResponse = postService.doReaction(postId, reactRequest);
 		return new ResponseEntity<>(reactResponse.getFirst(), reactResponse.getSecond());
 	}
-
+	
 	@GetMapping(value = "/post/{postId}/reactors")
 	public ResponseEntity<List<ProfileHead>> getReactorsByPostId(@PathVariable("postId") String postId) {
 		Pair<List<ProfileHead>, HttpStatus> profileHeadList = postService.getPostReactors(postId);
